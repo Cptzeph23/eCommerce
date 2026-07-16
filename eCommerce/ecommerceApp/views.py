e0fa4a165a636e96fa886b9baf0a6e8745b92193
Cptzeph23 <asekazephaniah@gmail.com>
2026-07-16 10:03:08 +0300

    static files

--- eCommerce/ecommerceApp/views.py
+++ eCommerce/ecommerceApp/views.py
@@ -1,6 +1,37 @@
+from urllib import request
+
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def chefs(request):
+    return render(request, 'chefs.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def orders(request):
+    return render(request, 'orders.html')
+
+def reservation(request):
+    return render(request, 'reservation.html')
+
+def reviews(request):
+    return render(request, 'about.html')
+
+def starter_page(request):
+    return render(request, 'starter-page.html')
+
+
+
+
+
