92774fc4ff739574b7a414c72f45e0d391fd501c
Cptzeph23 <asekazephaniah@gmail.com>
2026-07-27 11:19:23 +0300

    models

--- eCommerce/ecommerceApp/models.py
+++ eCommerce/ecommerceApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# model for login and registration
+
+class Member(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email   
+    
+# model for registration form
+class NewUser(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
